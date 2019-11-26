d46e81774ab6a509b41b3b95f27105b8189be28f
Francisco Javier Jurado Moreno <beetelbrox@gmail.com>
2019-11-26 10:45:46 +0100

    Finish floorplan implementation

--- AVLSI/99_project/src/floorplan.cpp
+++ AVLSI/99_project/src/floorplan.cpp
@@ -7,33 +7,30 @@ using namespace std;
 Floorplan::Floorplan(const vector<dim> &sf, int x, int y):
   Floorplan(&sf[0], sf.size(), x, y) {}
 
-Floorplan::Floorplan(const dim* sf, int n_shapes, int x, int y): dim_x{x}, _y{y} {
-    cout << sf << endl;
-    _shape_function = new dim[n_shapes];
-    cout <<"teste" << endl;
-    _limit = _shape_function + n_shapes;
-
-    copy(sf, sf+n_shapes, _shape_function);
+Floorplan::Floorplan(const dim* shapes, int n_shapes, int x, int y): _x(x), _y(y) {
+    _shapes = make_unique<dim[]>(n_shapes);
+    _limit = _shapes.get() + n_shapes;  // pointer to the end
+    copy(shapes, shapes+n_shapes, _shapes.get());
   }
 
-Floorplan::~Floorplan() { delete[] _shape_function; }
+Floorplan::dim* Floorplan::begin() const { return _shapes.get(); }
 
-// const std::vector<std::pair<int, int>>* Floorplan::get_shape_function () const {
-//   return &shape_function;
-// }
+Floorplan::dim* Floorplan::end() const { return _limit; }
 
-Floorplan::dim* Floorplan::begin() const{
-  return _shape_function;
+size_t Floorplan::size() const{ return _limit - _shapes.get(); }
+
+Floorplan::dim Floorplan::get_position() const { return {_x, _y}; }
+
+int Floorplan::get_x() const { return _x; }
+
+int Floorplan::get_y() const { return _y; }
+
+const Floorplan::dim* Floorplan::get_shape(size_t ix) const {
+  if (ix >= size()) return nullptr;
+  return _shapes.get()+ix;
 }
-Floorplan::dim* Floorplan::end() const{
-  return _limit;
-}
 
-size_t Floorplan::get_num_shapes() {
-  return _limit - _shape_function;
-}
-
-void Floorplan::print() {
+void Floorplan::print() const {
   cerr << "x = " << _x << ", y = " << _y << endl;
   for(dim* i=begin(); i !=end(); ++i) {
     cerr << "(" << i->first << "," << i->second << ") ";
--- AVLSI/99_project/src/floorplan.h
+++ AVLSI/99_project/src/floorplan.h
@@ -3,28 +3,28 @@
 
 #include <vector>
 #include <utility>
+#include <memory>
 
 class Floorplan {
   typedef std::pair<int, int> dim;
-  dim *_shape_function, *_limit;
+
+  std::unique_ptr<dim[]> _shapes;
+  dim* _limit;
   int _x, _y;
 
 public:
-
   explicit Floorplan(const std::vector<dim> &sf=std::vector<dim>(), int x=0, int y=0);
   Floorplan(const dim* sf, int num_shapes, int x=0, int y=0);
-  ~Floorplan();
 
   dim* begin() const;
   dim* end() const;
 
-  std::size_t get_num_shapes();
-
-  void print();
-
-  //const std::vector<std::pair<int, int>>* get_shape_function () const;
-
-  //std::pair<int, int> get_dim() const { return {x, y}; }
-
+  std::size_t size() const;
+  dim get_position() const;
+  int get_x() const;
+  int get_y() const;
+  const dim* get_shape(std::size_t ix) const;
+  void print() const;
+  
 };
 #endif
